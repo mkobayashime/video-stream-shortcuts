8b7a25d57c63478403496bb49ab65270afddb1ae
mkobayashime <m.kobayashi.me@gmail.com>
2020-04-20 23:10:44 +0900

    [Fix] Fix wrong if condition in background.js

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@ const matchUrl = "web.microsoftstream.com/video/"
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if(changeInfo.status == "complete") {
-    if(tab.url.indexOf(matchUrl)) {
+    if(tab.url.indexOf(matchUrl) !== -1) {
       chrome.tabs.sendMessage(tabId, {
         type: "msStreamUpdated"
       })
