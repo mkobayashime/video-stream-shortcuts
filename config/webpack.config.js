140bb217ca60016c80a2922e7324838a4d6bc7c1
mkobayashime <m.kobayashi.me@gmail.com>
2020-04-23 18:53:02 +0900

    [Chore] Run lint in config dir

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict"
 
-const merge = require('webpack-merge');
+const merge = require("webpack-merge")
 
-const common = require('./webpack.common.js');
-const PATHS = require('./paths');
+const common = require("./webpack.common.js")
+const PATHS = require("./paths")
 
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
-    background: PATHS.src + '/background.js',
-    msStream: PATHS.src + '/contentScripts/msStream.js',
-    primeVideo: PATHS.src + '/contentScripts/primeVideo.js',
-    ted: PATHS.src + '/contentScripts/ted.js',
-    popup: PATHS.src + '/popup.js'
+    background: PATHS.src + "/background.js",
+    msStream: PATHS.src + "/contentScripts/msStream.js",
+    primeVideo: PATHS.src + "/contentScripts/primeVideo.js",
+    ted: PATHS.src + "/contentScripts/ted.js",
+    popup: PATHS.src + "/popup.js",
   },
-});
+})
 
-module.exports = config;
+module.exports = config
