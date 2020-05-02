b3c59b67c388ba72c8528fbd7bf196f72ce19886
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-03 00:52:53 +0900

    [Update] Implement data binding between options page and chrome.storage

--- src/background.js
+++ src/background.js
@@ -2,6 +2,34 @@
 
 // eslint-disable-next-line no-undef
 chrome.runtime.onInstalled.addListener(() => {
+  const sitesConfigKeys = ["sites-prime-video", "sites-ted", "sites-ms-stream"]
+  const keysConfigKeys = [
+    "keys-k",
+    "keys-j",
+    "keys-l",
+    "keys-f",
+    "keys-m",
+    "keys-left-arrow",
+    "keys-right-arrow",
+  ]
+
+  const initSitesAndKeysConfig = (key) => {
+    // eslint-disable-next-line no-undef
+    chrome.storage.sync.get([key], (result) => {
+      if (result[key] === undefined) {
+        // eslint-disable-next-line no-undef
+        chrome.storage.sync.set({ [key]: true })
+      }
+    })
+  }
+
+  sitesConfigKeys.forEach((key) => {
+    initSitesAndKeysConfig(key)
+  })
+  keysConfigKeys.forEach((key) => {
+    initSitesAndKeysConfig(key)
+  })
+
   // eslint-disable-next-line no-undef
   chrome.storage.sync.get(["seek-sec"], (result) => {
     if (result["seek-sec"] === undefined) {
--- src/options/options.js
+++ src/options/options.js
@@ -1,3 +1,54 @@
 "use strict"
 
 import "./options.sass"
+
+window.onload = () => {
+  const checkboxesSites = Array.from(
+    document.getElementsByClassName("checkbox-sites")
+  )
+  const checkboxesKeys = Array.from(
+    document.getElementsByClassName("checkbox-keys")
+  )
+
+  const applySiteAndKeyConfig = (dom) => {
+    const key = dom.id
+    // eslint-disable-next-line no-undef
+    chrome.storage.sync.get([key], (result) => {
+      if (result[key] === false) {
+        dom.checked = false
+      }
+    })
+  }
+
+  checkboxesSites.forEach((dom) => {
+    applySiteAndKeyConfig(dom)
+  })
+  checkboxesKeys.forEach((dom) => {
+    applySiteAndKeyConfig(dom)
+  })
+
+  const watchSitesAndKeysConfig = (dom) => {
+    const key = dom.id
+    dom.addEventListener("change", (event) => {
+      // eslint-disable-next-line no-undef
+      chrome.storage.sync.set({ [key]: event.srcElement.checked })
+    })
+  }
+
+  checkboxesSites.forEach((dom) => {
+    watchSitesAndKeysConfig(dom)
+  })
+  checkboxesKeys.forEach((dom) => {
+    watchSitesAndKeysConfig(dom)
+  })
+
+  const seekSecInput = document.getElementById("seek-sec")
+  // eslint-disable-next-line no-undef
+  chrome.storage.sync.get(["seek-sec"], (result) => {
+    seekSecInput.value = result["seek-sec"]
+  })
+  seekSecInput.addEventListener("change", (event) => {
+    // eslint-disable-next-line no-undef
+    chrome.storage.sync.set({ "seek-sec": Number(event.srcElement.value) })
+  })
+}
