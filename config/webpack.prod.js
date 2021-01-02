9b27c5443be7f61edf0043d016dd98dc52545f17
Masaki Kobayashi <contact@mkobayashi.me>
2021-01-03 02:21:31 +0900

    [Chore] Replace bundle script with zip-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,22 @@
 "use strict"
 
+const path = require("path")
 const merge = require("webpack-merge")
 
 const common = require("./webpack.common.js")
 
+const ZipPlugin = require("zip-webpack-plugin")
+
+const version = require("../package.json").version
+
 // Merge webpack configuration files
-const config = merge(common, {})
+const config = merge(common, {
+  plugins: [
+    new ZipPlugin({
+      path: path.resolve(__dirname, "../"),
+      filename: `${version}`,
+    }),
+  ],
+})
 
 module.exports = config
