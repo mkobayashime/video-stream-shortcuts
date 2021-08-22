3aacbbdee4aa19fae539effd01f4bb6ae2ae0048
Masaki Kobayashi <contact@mkobayashi.me>
2021-08-22 22:13:15 +0900

    Run Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,32 @@
 module.exports = {
   parserOptions: {
-    "ecmaVersion": 2019,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
-    }
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     node: true,
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "prettier"
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
   rules: {
-    "eqeqeq": "error",
+    eqeqeq: "error",
     "no-console": "warn",
     "prettier/prettier": [
       "error",
       {
-        "endOfLine": "lf",
-        "semi": false,
-        "singleQuote": false,
-        "tabWidth": 2,
-        "trailingComma": "es5"
-      }
-    ]
+        endOfLine: "lf",
+        semi: false,
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: "es5",
+      },
+    ],
   },
   globals: {
-    "chrome": true
-  }
+    chrome: true,
+  },
 }
