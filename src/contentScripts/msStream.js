e1f425350a58b1eff9fdf149861b3c60c49e5578
Masaki Kobayashi <contact@mkobayashi.me>
2021-10-29 02:22:53 +0900

    Refactor MS Stream watching logic for improved stability

--- src/contentScripts/msStream.js
+++ src/contentScripts/msStream.js
@@ -6,133 +6,155 @@ import seek from "../methods/seek"
 import toggleMute from "../methods/toggleMute"
 import isTyping from "../methods/isTyping"
 import changePlaybackSpeed from "../methods/changePlaybackSpeed"
-import applyDefaultPlaybackSpeed from "../methods/applyDefaultPlaybackSpeed"
 import createIndicator from "../methods/createIndicator"
 
-// Run getVideo() when background.js sent "updated" message
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "updated") {
-    // Check if MS Stream is enabled in setting
-    loadConfig().then((result) => {
-      if (result["sites-ms-stream"]) {
-        getVideo(result)
-      }
-    })
+class MsStreamHandler {
+  constructor({ config }) {
+    this.media = null
+    this.config = config
+    this.preVolume = 1
   }
-})
 
-const getVideo = (config) => {
-  const promise = new Promise((resolve) => {
-    const interval = window.setInterval(() => {
+  watch() {
+    this.findMedia()
+    this.watchShortcuts(this.media, this.config)
+  }
+
+  findMedia() {
+    window.setInterval(() => {
       const media = document.getElementsByTagName("video")[0]
-      if (media) {
-        window.clearInterval(interval)
-        resolve(media)
+
+      if (media !== this.media) {
+        this.media = media
+
+        if (this.media) {
+          this.applyDefaultPlaybackSpeed()
+        }
       }
     }, 250)
-  })
-
-  promise.then((media) => {
-    setShortcuts(media, config)
-    applyDefaultPlaybackSpeed(media, "speed-ms-stream")
-  })
-}
-
-const setShortcuts = (media, config) => {
-  let preVolume
-
-  // Use default fullscreen button on the page instead of /methods/toggleFullscreen.js
-  // in order not to make the video controls invisible in fullscreen mode
-  const toggleFullscreen = () => {
-    document.getElementsByClassName("vjs-fullscreen-control")[0].click()
   }
 
-  document.onkeyup = (e) => {
-    if (!isTyping()) {
-      switch (e.key) {
-        case "k":
-          if (config["keys-k"]) {
-            togglePause(media)
-            callIndicatorCreator({ type: "icon", id: "togglePause", media })
-          }
-          break
-        case "j":
-          if (config["keys-j"]) {
-            seek({
-              media: media,
-              direction: "backward",
-              seekSec: config["seek-sec"],
-            })
-            callIndicatorCreator({ type: "icon", id: "seekBackward" })
-          }
-          break
-        case "l":
-          if (config["keys-l"]) {
-            seek({
-              media: media,
-              direction: "forward",
-              seekSec: config["seek-sec"],
-            })
-            callIndicatorCreator({ type: "icon", id: "seekForward" })
-          }
-          break
-        case "f":
-          if (config["keys-f"]) {
-            toggleFullscreen()
-          }
-          break
-        case "m":
-          if (config["keys-m"]) {
-            preVolume = toggleMute(media, preVolume)
-            if (media.volume !== 0) {
-              callIndicatorCreator({
-                type: "text",
-                text: Math.round(media.volume * 100).toString() + "%",
+  applyDefaultPlaybackSpeed() {
+    const loadedAndPlayed = Promise.all([
+      new Promise((resolve) => {
+        this.media.addEventListener("loadeddata", () => resolve())
+      }),
+      new Promise((resolve) => {
+        this.media.addEventListener("play", () => resolve())
+      }),
+    ])
+
+    loadedAndPlayed.then(() => {
+      global.setTimeout(() => {
+        this.media.playbackRate = this.config["speed-ms-stream"]
+      }, 500)
+    })
+  }
+
+  callIndicatorCreator({ type, id, text, media }) {
+    const wrapper = document.getElementsByTagName("video")[0].parentNode
+    wrapper.style.position = "absolute"
+    wrapper.style.width = "100%"
+    wrapper.style.height = "100%"
+    wrapper.style.top = "0"
+    wrapper.style.left = "0"
+
+    createIndicator({
+      type,
+      id,
+      text,
+      wrapper,
+      media,
+    })
+  }
+
+  watchShortcuts() {
+    document.onkeyup = (e) => {
+      if (!this.media) return
+
+      if (!isTyping()) {
+        switch (e.key) {
+          case "k":
+            if (this.config["keys-k"]) {
+              togglePause(this.media)
+              this.callIndicatorCreator({
+                type: "icon",
+                id: "togglePause",
+                media: this.media,
               })
-            } else {
-              callIndicatorCreator({ type: "icon", id: "mute" })
             }
+            break
+          case "j":
+            if (this.config["keys-j"]) {
+              seek({
+                media: this.media,
+                direction: "backward",
+                seekSec: this.config["seek-sec"],
+              })
+              this.callIndicatorCreator({ type: "icon", id: "seekBackward" })
+            }
+            break
+          case "l":
+            if (this.config["keys-l"]) {
+              seek({
+                media: this.media,
+                direction: "forward",
+                seekSec: this.config["seek-sec"],
+              })
+              this.callIndicatorCreator({ type: "icon", id: "seekForward" })
+            }
+            break
+          case "f":
+            if (this.config["keys-f"]) {
+              // Use default fullscreen button on the page instead of /methods/toggleFullscreen.js
+              // in order not to make the video controls invisible in fullscreen mode
+              document
+                .getElementsByClassName("vjs-fullscreen-control")[0]
+                .click()
+            }
+            break
+          case "m":
+            if (this.config["keys-m"]) {
+              this.preVolume = toggleMute(this.media, this.preVolume)
+              if (this.media.volume !== 0) {
+                this.callIndicatorCreator({
+                  type: "text",
+                  text: Math.round(this.media.volume * 100).toString() + "%",
+                })
+              } else {
+                this.callIndicatorCreator({ type: "icon", id: "mute" })
+              }
+            }
+            break
+          case "<": {
+            if (this.config["keys-left-arrow"]) {
+              const curSpeed = changePlaybackSpeed(this.media, "decrease")
+              this.callIndicatorCreator({
+                type: "text",
+                text: curSpeed.toString() + "x",
+              })
+            }
+            break
           }
-          break
-        case "<": {
-          if (config["keys-left-arrow"]) {
-            const curSpeed = changePlaybackSpeed(media, "decrease")
-            callIndicatorCreator({
-              type: "text",
-              text: curSpeed.toString() + "x",
-            })
+          case ">": {
+            if (this.config["keys-right-arrow"]) {
+              const curSpeed = changePlaybackSpeed(this.media, "increase")
+              this.callIndicatorCreator({
+                type: "text",
+                text: curSpeed.toString() + "x",
+              })
+            }
+            break
           }
-          break
-        }
-        case ">": {
-          if (config["keys-right-arrow"]) {
-            const curSpeed = changePlaybackSpeed(media, "increase")
-            callIndicatorCreator({
-              type: "text",
-              text: curSpeed.toString() + "x",
-            })
-          }
-          break
         }
       }
     }
   }
 }
 
-// Page specific wrapper of methods/createIndicator.js
-const callIndicatorCreator = ({ type, id, text, media }) => {
-  const wrapper = document.getElementsByTagName("video")[0].parentNode
-  wrapper.style.position = "absolute"
-  wrapper.style.width = "100%"
-  wrapper.style.height = "100%"
-  wrapper.style.top = "0"
-  wrapper.style.left = "0"
-
-  createIndicator({
-    type,
-    id,
-    text,
-    wrapper,
-    media,
-  })
-}
+loadConfig().then((config) => {
+  if (config["sites-ms-stream"]) {
+    const handler = new MsStreamHandler({ config })
+    handler.watch()
+  }
+})
