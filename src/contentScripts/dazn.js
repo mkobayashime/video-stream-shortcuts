9ba92a4334d04b7de2d0772d7d56cadc71ca1d56
Masaki Kobayashi <contact@mkobayashi.me>
2021-11-11 01:06:40 +0900

    Refactor the way to find video elements in DAZN

--- src/contentScripts/dazn.js
+++ src/contentScripts/dazn.js
@@ -5,94 +5,101 @@ import seek from "../methods/seek"
 import isTyping from "../methods/isTyping"
 import changePlaybackSpeed from "../methods/changePlaybackSpeed"
 
-window.onload = () => {
-  // Check if DAZN is enabled in setting
-  loadConfig().then((result) => {
-    if (result["sites-dazn"]) {
-      getVideo(result)
-    }
-  })
-}
+class daznHandler {
+  constructor({ config }) {
+    this.media = null
+    this.config = config
+  }
 
-const getVideo = (config) => {
-  const promise = new Promise((resolve) => {
-    const interval = window.setInterval(() => {
+  watch() {
+    this.findMedia()
+    this.listenShortcuts()
+  }
+
+  findMedia() {
+    window.setInterval(() => {
       const media = document.getElementsByTagName("video")[0]
-      if (media) {
-        window.clearInterval(interval)
-        resolve(media)
+
+      if (media !== this.media) {
+        this.media = media
       }
     }, 250)
-  })
+  }
 
-  promise.then((media) => {
-    setShortcuts(media, config)
-  })
-}
+  listenShortcuts() {
+    document.onkeyup = (e) => {
+      if (!this.media) return
 
-const setShortcuts = (media, config) => {
-  document.onkeyup = (e) => {
-    if (!isTyping()) {
-      switch (e.key) {
-        case "k":
-          if (config["keys-k"]) {
-            const togglePauseButton =
-              document.querySelector(
-                "button[data-test-id*='PLAYER_BUTTON_PLAY']"
-              ) ||
-              document.querySelector(
-                "button[data-test-id*='PLAYER_BUTTON_PAUSE']"
-              )
-            if (togglePauseButton) togglePauseButton.click()
+      if (!isTyping()) {
+        switch (e.key) {
+          case "k":
+            if (this.config["keys-k"]) {
+              const togglePauseButton =
+                document.querySelector(
+                  "button[data-test-id*='PLAYER_BUTTON_PLAY']"
+                ) ||
+                document.querySelector(
+                  "button[data-test-id*='PLAYER_BUTTON_PAUSE']"
+                )
+              if (togglePauseButton) togglePauseButton.click()
+            }
+            break
+          case "j":
+            if (this.config["keys-j"]) {
+              seek({
+                media: this.media,
+                direction: "backward",
+                seekSec: this.config["seek-sec"],
+              })
+            }
+            break
+          case "l":
+            if (this.config["keys-l"]) {
+              seek({
+                media: this.media,
+                direction: "forward",
+                seekSec: this.config["seek-sec"],
+              })
+            }
+            break
+          case "f":
+            if (this.config["keys-f"]) {
+              const fullscreenButton = document.getElementsByClassName(
+                "fullscreen___fullscreen___1OXBx"
+              )[0]
+              if (fullscreenButton) fullscreenButton.click()
+            }
+            break
+          case "m":
+            if (this.config["keys-m"]) {
+              const muteButton = document.getElementsByClassName(
+                "volumeControl___volume-mute-unmute___28Jvb"
+              )[0]
+              if (muteButton) muteButton.click()
+            }
+            break
+          case "<": {
+            if (this.config["keys-left-arrow"]) {
+              changePlaybackSpeed(this.media, "decrease")
+            }
+            break
           }
-          break
-        case "j":
-          if (config["keys-j"]) {
-            seek({
-              media,
-              direction: "backward",
-              seekSec: config["seek-sec"],
-            })
+          case ">": {
+            if (this.config["keys-right-arrow"]) {
+              changePlaybackSpeed(this.media, "increase")
+            }
+            break
           }
-          break
-        case "l":
-          if (config["keys-l"]) {
-            seek({
-              media,
-              direction: "forward",
-              seekSec: config["seek-sec"],
-            })
-          }
-          break
-        case "f":
-          if (config["keys-f"]) {
-            const fullscreenButton = document.getElementsByClassName(
-              "fullscreen___fullscreen___1OXBx"
-            )[0]
-            if (fullscreenButton) fullscreenButton.click()
-          }
-          break
-        case "m":
-          if (config["keys-m"]) {
-            const muteButton = document.getElementsByClassName(
-              "volumeControl___volume-mute-unmute___28Jvb"
-            )[0]
-            if (muteButton) muteButton.click()
-          }
-          break
-        case "<": {
-          if (config["keys-left-arrow"]) {
-            changePlaybackSpeed(media, "decrease")
-          }
-          break
-        }
-        case ">": {
-          if (config["keys-right-arrow"]) {
-            changePlaybackSpeed(media, "increase")
-          }
-          break
         }
       }
     }
   }
 }
+
+// Check if DAZN is enabled in setting
+loadConfig().then((config) => {
+  if (config["sites-dazn"]) {
+    const handler = new daznHandler({ config })
+    handler.watch()
+  }
+})
