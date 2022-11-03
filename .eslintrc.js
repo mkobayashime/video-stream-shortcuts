985d9d04fe1078359343128c405b7d3ebc5782f1
Masaki Kobayashi <contact@mkobayashi.me>
2022-11-03 21:33:49 +0900

    Make use of `@mkobayashime/eslint-config`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,9 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    node: true,
-    browser: true,
-    es6: true,
-  },
-  extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
-  rules: {
-    eqeqeq: "error",
-    "no-console": "warn",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "lf",
-        semi: false,
-        singleQuote: false,
-        tabWidth: 2,
-        trailingComma: "es5",
-      },
-    ],
-  },
+  env: { es2021: true, node: true, browser: true },
+  extends: ["@mkobayashime"],
+  plugins: ["import"],
   globals: {
     chrome: true,
   },
+  rules: {},
 }
